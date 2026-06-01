5c11cb2189fa6af7c06a9d895d9ed33641212ddf
tqtdhd-ux <tqtdhd@gmail.com>
2026-06-01 23:01:34 +0700

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,6 @@
 window.APP_CONFIG = {
-  supabaseUrl: "https://lfjmvrujjehqtxbnzrnz.supabase.co/rest/v1/",
+  supabaseUrl: "https://lfjmvrujjehqtxbnzrnz.supabase.co",
   supabaseKey: "sb_publishable_NvWk2Z3cX2yiVJ4rwGz_Gw_ivRIqsDE",
+  publicSiteUrl: "YOUR_GITHUB_PAGES_URL",
   classCode: "DLS-K01"
 };
